c37756c2ad00965f9da054121670106ddb0fcee1
Slumbali786 <mohomedhoosen232@gmail.com>
2023-12-05 17:57:51 +0200

    Update RUHome.php

---
+++ RUHome.php
@@ -0,0 +1,119 @@
+<html>
+<head>
+    <title>TalentForge</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 0px;
+        }
+
+        h1 {
+            font-size: 66px;
+            font-weight: bold;
+            font-family: 'ALGERIAN', sans-serif;
+            margin-bottom: 0px;
+        }
+
+        #logo {
+            width: 430px; /* Adjust the width as needed */
+            height: auto;
+        }
+
+        .tile:nth-child(1) {
+            background-color: #e6ccff; /* Light Purple */
+        }
+
+        .tile:nth-child(2) {
+            background-color: #ffb3e6; /* Pink */
+        }
+
+        .tile:nth-child(3) {
+            
+            background-color: #b3e0ff; /* Light Blue */
+        }
+
+        .tile:nth-child(4) {
+           
+            background-color: #ffff00; /* Orange */
+        }
+
+        .tile:nth-child(5) {
+            background-color: #cc9900; /* Dark Yellow */
+
+        }
+
+        .tile:nth-child(6) {
+            background-color: #ffddb3; /* Light Orange */
+        }
+
+        .tile {
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            padding: 20px;
+            margin-bottom: 20px;
+            text-align: left;
+        }
+
+
+    table label {
+        font-size: 24px; /* Adjust the font size as needed */
+    }
+
+    table button {
+        font-size: 30px;
+    }
+
+    .btn-md {
+        font-size: 34px; /* Adjust the font size to make the button bigger */
+        padding: 10px 20px; /* Adjust padding for width and height */
+    }
+
+    .nav-item {
+        list-style-type: none;
+    }
+	
+        header {
+            background-color: #333;
+            color: white;
+            padding: 10px;
+            text-align: center;
+        }
+
+        nav {
+            display: flex;
+            justify-content: space-around;
+            background-color: #444;
+            padding: 10px;
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+        }
+
+        nav a:hover {
+            text-decoration: underline;
+        }
+
+    </style>
+</head>
+<header>
+  <nav>
+        <a href="RUHome.php">Home</a>
+        <a href="RegUserAccountPage.php">My Account</a>
+		<a href="CreateCVs.php">Create CV</a>
+		<a href="ViewCVs.php">My CVs</a>
+		<a href="ApplyJobs.php">Jobs</a>
+		<a href="AppliedJobs.php">Applications</a>
+    </nav>
+    </header>
+<body>
+    <h1>TalentForge</h1>
+    <img src="TalentForge.png" alt="TalentForge Image">
+    <p>Welcome to TalentForge, where innovation and efficiency converge in the realm of talent acquisition! Our revolutionary web-based platform is designed to transform your recruitment experience. Seamlessly connecting job seekers and employers, TalentForge streamlines every facet of the job application and hiring process. Join us in this exciting journey towards a future of efficient, sustainable, and connected recruitment. Your success starts here!</p>
+   
+</body>
+</html>
+
+
